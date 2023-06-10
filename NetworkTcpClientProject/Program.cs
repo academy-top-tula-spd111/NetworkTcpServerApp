63e88cdcdc8c2c02d2a6d8cae08c24b7d64fe4a1
maxefimov <efimov.top.academy@gmail.com>
2023-06-10 17:27:41 +0300

    10.06.23

--- NetworkTcpClientProject/Program.cs
+++ NetworkTcpClientProject/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Sockets;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 using Socket socketClient = new Socket(AddressFamily.InterNetwork, 
                                        SocketType.Stream, 
@@ -6,8 +8,22 @@ using Socket socketClient = new Socket(AddressFamily.InterNetwork,
 
 try
 {
-    await socketClient.ConnectAsync("127.0.0.1", 8888);
+    await socketClient.ConnectAsync(IPAddress.Loopback, 8888);
     Console.WriteLine($"\tConnect with server: {socketClient.RemoteEndPoint}");
+    byte[] buffer = new byte[1024];
+    int bites = await socketClient.ReceiveAsync(buffer);
+    string message = Encoding.UTF8.GetString(buffer);
+
+    Console.WriteLine($"Message from server: {message}");
+
+    Thread.Sleep(1000);
+    message = "Hello server! " + DateTime.Now.ToLongTimeString() + "\nAppend text";
+    buffer = Encoding.UTF8.GetBytes(message);
+    
+    Console.WriteLine();
+    Console.WriteLine($"Message to server: {message}");
+    await socketClient.SendAsync(BitConverter.GetBytes(buffer.Length));
+    await socketClient.SendAsync(buffer);
 }
 catch (SocketException e)
 {
--- NetworkTcpServerApp/Program.cs
+++ NetworkTcpServerApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 8888);
 
@@ -7,9 +8,55 @@ using Socket socketServer = new Socket(AddressFamily.InterNetwork, SocketType.St
 
 socketServer.Bind(ipEndPoint);
 Console.WriteLine(socketServer.LocalEndPoint);
-socketServer.Listen(1000);
+socketServer.Listen();
 Console.WriteLine("Server listen...");
 
-using Socket socketClient = await socketServer.AcceptAsync();
+while(true)
+{
+    using Socket socketClient = await socketServer.AcceptAsync();
+    Console.WriteLine($"Client address: {socketClient.RemoteEndPoint}");
 
-Console.WriteLine($"Client address: {socketClient.RemoteEndPoint}");
+    string message = "Hello client! " + DateTime.Now.ToLongTimeString();
+    byte[] buffer = Encoding.UTF8.GetBytes(message);
+    await socketClient.SendAsync(buffer);
+
+    Console.WriteLine($"Message to client: {message}");
+
+
+
+    byte[] sizeMessage = new byte[4];
+    await socketClient.ReceiveAsync(sizeMessage);
+
+    byte[] bufferMessage = new byte[BitConverter.ToInt32(sizeMessage)];
+    await socketClient.ReceiveAsync(bufferMessage);
+
+    Console.WriteLine($"Message from client: {Encoding.UTF8.GetString(bufferMessage)}");
+
+
+    /*
+    StringBuilder response = new StringBuilder();
+    var bufferByte = new byte[1];
+    List<byte> bufferList = new List<byte>();
+
+    while(true)
+    {
+        int count = await socketClient.ReceiveAsync(bufferByte);
+        if (count == 0 || bufferByte[0] == '\n') break;
+        bufferList.Add(bufferByte[0]);
+    }
+
+    Console.WriteLine($"Message from client (with marker): {Encoding.UTF8.GetString(bufferList.ToArray())}");
+    */
+
+    //int bytes;
+    //do
+    //{
+    //    bytes = await socketClient.ReceiveAsync(buffer);
+    //    response.Append(Encoding.UTF8.GetString(buffer));
+        
+    //} while (bytes > 0);
+    //Console.WriteLine($"Response from client: {response}");
+}
+
+
+
