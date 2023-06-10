c017a61e4cdac67ea1724b5b165864d23d715a13
maxefimov <efimov.top.academy@gmail.com>
2023-06-10 15:05:08 +0300

    Добавьте файлы проекта.

---
+++ NetworkTcpClientProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Sockets;
+
+using Socket socketClient = new Socket(AddressFamily.InterNetwork, 
+                                       SocketType.Stream, 
+                                       ProtocolType.Tcp);
+
+try
+{
+    await socketClient.ConnectAsync("127.0.0.1", 8888);
+    Console.WriteLine($"\tConnect with server: {socketClient.RemoteEndPoint}");
+}
+catch (SocketException e)
+{
+    Console.WriteLine($"not connect: {e.Message}");
+}
---
+++ NetworkTcpServerApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 8888);
+
+using Socket socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+socketServer.Bind(ipEndPoint);
+Console.WriteLine(socketServer.LocalEndPoint);
+socketServer.Listen(1000);
+Console.WriteLine("Server listen...");
+
+using Socket socketClient = await socketServer.AcceptAsync();
+
+Console.WriteLine($"Client address: {socketClient.RemoteEndPoint}");
